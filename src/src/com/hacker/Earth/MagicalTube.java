0253630815b111950c69a3a496d60b448cbf1b78
qaprayogshala <qaprayogshala@gmail.com>
2022-07-03 14:27:47 +0530

    Update MagicalTube.java

--- src/src/com/hacker/Earth/MagicalTube.java
+++ src/src/com/hacker/Earth/MagicalTube.java
@@ -1,31 +1,40 @@
-package com.hacker.Earth;
+package com.basics.hackerEarth;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MagicalTube {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int Q = scanner.nextInt();
-        int X;
-        int N;
-        List<Integer> list = new ArrayList<>();
+        int T = scanner.nextInt();
+        int Q = 0;
+        int X = 0;
+        int N = 0;
+        List<Integer> actualList;
         List<Integer> outputList = new ArrayList<>();
 
+        for (int i = 0; i < T; i++) {
+            actualList = new ArrayList<>();
+            Q = scanner.nextInt();
+            for (int j = 0; j < Q; j++) {
+                X = scanner.nextInt();
+                N = scanner.nextInt();
 
-        for (int i = 0; i < Q; i++) {
-            X = scanner.nextInt();
-            N = scanner.nextInt();
-            if (X == 1) {
-                list.add(i, N);
-            } else if (X == 2) {
-                list.add(i, N);
-                outputList.add((list.get(i - 1)));
+                if (X == 1) {
+                    actualList.add(j, N);
+                } else if (X == 2) {
+                    if (j != 0) {
+                        actualList.add(j, N);
+                        outputList.add(actualList.get(j - 1));
+                    }
+                }
             }
+
         }
-        for (int i : outputList) {
-            System.out.print(i + " ");
+        for (int k : outputList) {
+            System.out.print(k + " ");
         }
     }
 }
